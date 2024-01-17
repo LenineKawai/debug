3e5cde4d88dce4124a225532ec2a2e0387227b9a
LenineKawai <max.golinvaux@gmail.com>
2024-01-17 12:58:25 +0100

    incomplete work

---
+++ ex04/leaks_in_a_bottle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (int ac, char **av)
+{
+    int i = 1;
+    char **str_array = malloc(ac * sizeof(str_array));
+    while (i < ac)
+    {
+        str_array[i] = substr
+    }
+}
